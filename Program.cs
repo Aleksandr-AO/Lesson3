2fd56b203d547817ae73b69f392e9822302f2040
Aleksandr <lepalovskiy2015@mail.ru>
2022-11-21 11:57:57 +0700

    Lesson3/1

--- Program.cs
+++ Program.cs
@@ -107,8 +107,8 @@ Console.WriteLine();
 newText = Replase(newText, 'с', 'С');
 Console.WriteLine(newText);
 
-====== Упорядочение данных внутри массива
-*/
+====== Упорядочение данных внутри массива (Задание: Упорядочить массив от минимального к максимальному)
+
 int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 void PrintArray(int [] array)
 {
@@ -140,3 +140,38 @@ void SelectionSort(int [] array)
 PrintArray(arr);
 SelectionSort(arr);
 PrintArray(arr);
+
+====== Упорядочение данных внутри массива (Задание: Упорядочить массив от максимального к минимальному)
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int [] array)
+{
+    
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+*/
